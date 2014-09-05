f842af1175a0548202c9d8a68e1bc12289b25181
Konrad Borowski <x.fix@o2.pl>
2014-09-05 22:08:13 +0200

    Use two dictionaries for guess game.

--- word_game.rb
+++ word_game.rb
@@ -1,4 +1,5 @@
 require 'cinch'
+require 'set'
 
 module Cinch::Plugins
   class WordGame
@@ -6,14 +7,15 @@ module Cinch::Plugins
 
     def initialize(*)
       super
-      @dict = File.readlines('dict.txt').map(&:chomp)
+      @words_dict = File.readlines('dict.txt').map(&:chomp).to_set
+      @words_to_guess_dict = File.readlines('words_to_guess.txt').map(&:chomp)
     end
 
     match(/wordgame start/, method: :start)
     def start(m)
       unless @started
         @started = true
-        @word = @dict.sample
+        @word = @words_to_guess_dict.sample
         m.reply("#{m.user}: New word game started! I'm thinking of a word, use !guess (word) to guess.")
       end
     end
@@ -22,9 +24,17 @@ module Cinch::Plugins
     def guess(m, guess)
       return unless @started
 
-      if guess.downcase < @word.downcase
+      lowercase_guess = guess.downcase
+      lowercase_word = @word.downcase
+
+      unless @words_dict.include?(lowercase_guess)
+        m.reply("#{m.user.nick}: #{guess} is not in my dictionary.")
+        return
+      end
+
+      if lowercase_guess < lowercase_word
         m.reply("#{m.user.nick}: #{guess} comes before the word in the dictionary.")
-      elsif guess.downcase > @word.downcase
+      elsif lowercase_guess > lowercase_word
         m.reply("#{m.user.nick}: #{guess} comes after the word in the dictionary.")
       else
         m.reply("#{m.user.nick}: #{guess} is correct! #{m.user.nick} is the winner! Congratulations!")
